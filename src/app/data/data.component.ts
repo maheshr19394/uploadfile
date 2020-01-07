22daa35657c226775a3c8df01dc1827f323e3a05
Mahesh Rapolu <mr19394@corp.ojas-it.com>
2020-01-07 14:15:36 +0530

    uploadfile

---
+++ src/app/data/data.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-data',
+  templateUrl: './data.component.html',
+  styleUrls: ['./data.component.css']
+})
+export class DataComponent implements OnInit {
+  fileContent;
+  constructor() { }
+
+  ngOnInit() {
+  }
+  public onChange(fileList: FileList): void {
+    let file = fileList[0];
+    let fileReader: FileReader = new FileReader();
+    let self = this;
+    fileReader.onloadend = function(x) {
+      self.fileContent = fileReader.result;
+    }
+    fileReader.readAsText(file);
+  }
+}
